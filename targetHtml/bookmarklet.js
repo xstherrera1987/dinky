37734ffcc9cc9c254cbec096a9444a566f024cb4
Joe Herrera <xstherrera1987@gmail.com>
2013-06-27 01:22:10 -0700

    update code page

--- targetHtml/bookmarklet.js
+++ targetHtml/bookmarklet.js
@@ -1,19 +1,20 @@
-// copy into JS console
+/** **************************************************************************** */
 (function() {
 	var head = document.getElementsByTagName('head')[0];
 	var script = document.createElement('script');
 	script.type = 'text/javascript';
 	script.src = 'http://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js';
 	head.appendChild(script);
-	setTimeout( function() { 
+	setTimeout(function() {
 		if (jQuery) {
-			alert ("jQuery loaded");
+			// some browsers use $ for their own JS tools
+			$ = jQuery;
+			alert("jQuery loaded");
 		} else {
-			alert ("jQuery loading failed");
+			alert("jQuery loading failed");
 		}
-	},1500);
+	}, 1500);
 })();
-
-
+/** **************************************************************************** */
 // minified (save as bookmark)
-javascript:(function(){var e=document.getElementsByTagName("head")[0];var t=document.createElement("script");t.type="text/javascript";t.src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js";e.appendChild(t);setTimeout(function(){if(jQuery){alert("jQuery loaded")}else{alert("jQuery loading failed")}},1500)})()
+javascript:(function(){var e=document.getElementsByTagName("head")[0];var t=document.createElement("script");t.type="text/javascript";t.src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js";e.appendChild(t);setTimeout(function(){if(jQuery){$=jQuery;alert("jQuery loaded")}else{alert("jQuery loading failed")}},1500)})()
