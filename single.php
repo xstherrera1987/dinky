354f88f6f9e4aa5a942183a5b21abfc8d307fecc
Joe Herrera <xstherrera1987@gmail.com>
2013-07-10 22:02:12 -0700

    update post template pagination

--- single.php
+++ single.php
@@ -6,16 +6,20 @@
 	<article>
 	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-		<span class="previousPost"><?php previous_post_link( '%link', '&larr; Older Posts'); ?></span>
-		<span class="divider"> | </span>
-		<span class="nextPost"><?php next_post_link( '%link', 'Newer Posts &rarr;'); ?></span>
+		<nav>
+			<span class="postNav"><?php previous_post_link( '%link', '&larr; Older Posts'); ?></span>
+			<span class="divider"> | </span>
+			<span class="postNav"><?php next_post_link( '%link', 'Newer Posts &rarr;'); ?></span>
+		</nav>
 		
 		<h1><?php the_title() ?></h1>
 		<?php the_content(); ?>
 		
-		<span class="previousPost"><?php previous_post_link( '%link', '&larr; Older Posts'); ?></span>
-		<span class="divider"> | </span>
-		<span class="nextPost"><?php next_post_link( '%link', 'Newer Post &rarr;'); ?></span>
+		<nav>
+			<span class="postNav"><?php previous_post_link( '%link', '&larr; Older Posts'); ?></span>
+			<span class="divider"> | </span>
+			<span class="postNav"><?php next_post_link( '%link', 'Newer Posts &rarr;'); ?></span>
+		</nav>
 
 	<?php endwhile; endif; ?>	
 	</article>
